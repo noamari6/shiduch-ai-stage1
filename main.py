6ac712641c279155b4b2e45fb7df60420af8d84a
noamari6 <noamari6@gmail.com>
2025-06-27 19:23:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import os  # צריך בשביל לקרוא את משתנה הסביבה PORT
 
 app = Flask(__name__)
 
@@ -11,22 +12,4 @@ def form():
         age = request.form["age"]
         city = request.form["city"]
         religious_level = request.form["religious_level"]
-        about = request.form["about"]
-
-        profile = {
-            "full_name": full_name,
-            "age": age,
-            "city": city,
-            "religious_level": religious_level,
-            "about": about
-        }
-
-        profiles.append(profile)
-        return render_template("profiles.html", profiles=profiles)
-
-    return render_template("form.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+        about =
